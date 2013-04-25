d7de11dee3ebf157b7d340b3a0119348af2e7b6d
Jarrah Watson <jarrah@argos.io>
2013-04-25 19:22:52 +1000

    Configure bootstrap theme

--- conf.py
+++ conf.py
@@ -97,7 +97,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #html_theme = 'default'
 html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme_path = [ '_themes/sphinx_bootstrap_theme' ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
