c13120a7a28cf8ff70df17213d29290628d87d40
nick <nick@argos.io>
2013-12-15 18:11:31 +1100

    theme changes

--- source/conf.py
+++ source/conf.py
@@ -98,7 +98,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_themex'
+html_theme = 'sphinx_eagleio_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -107,10 +107,10 @@ html_theme = 'sphinx_rtd_themex'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
+html_theme_path = ["_themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_theme_path = ["_themes"]
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
