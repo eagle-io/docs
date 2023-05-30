f8f50790421ff09b3b5239cedd3f4c7291927b65
Nicholas Hitchins <nick.hitchins@gmail.com>
2023-05-30 14:46:53 +1000

    Update copyright notice and add links to footer

--- source/conf.py
+++ source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'eagle.io'
-copyright = u'2021 Argos.io Pty Ltd'
+copyright = u'2023 Bentley Systems, Incorporated. All rights reserved'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -195,7 +195,7 @@ latex_elements = {
 #'papersize': 'letterpaper',
 
 # remove release label from doc.
-'releasename': '2021',
+'releasename': '2023',
 
 # The font size ('10pt', '11pt' or '12pt').
 'pointsize': '11pt',
@@ -210,7 +210,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'eagleio.tex', u'eagle.io documentation',
-   u'Argos.io Pty Ltd', 'manual'),
+   u'Bentley Systems, Incorporated', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -240,7 +240,7 @@ latex_show_urls = True  #'inline'
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'eagleio', u'eagle.io documentation',
-     [u'Argos.io Pty Ltd'], 1)
+     [u'Bentley Systems, Incorporated'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -254,7 +254,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'eagleio', u'eagle.io documentation',
-   u'Argos.io Pty Ltd', 'eagleio', 'One line description of project.',
+   u'Bentley Systems, Incorporated', 'eagleio', 'One line description of project.',
    'Miscellaneous'),
 ]
 
